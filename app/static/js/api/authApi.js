2edf57e08c3e61abac7daece111cb4d60a706acd
Sodaqw <art.sychev9@gmail.com>
2026-05-13 14:50:40 +0400

    Update authApi.js

--- app/static/js/api/authApi.js
+++ app/static/js/api/authApi.js
@@ -1,37 +1,29 @@
 const API_URL = 'https://okorochki-est.onrender.com/api';
-
+//Функция для регистрации пользователя
 export async function registerUser(data) {
-
     const response = await fetch(
         `${API_URL}/register`,
         {
             method: 'POST',
-
             headers: {
                 'Content-Type': 'application/json'
             },
-
             body: JSON.stringify(data)
         }
     );
-
     return await response.json();
 }
-
+//Функция для входа пользователя
 export async function loginUser(data) {
-
     const response = await fetch(
         `${API_URL}/login`,
         {
             method: 'POST',
-
             headers: {
                 'Content-Type': 'application/json'
             },
-
             body: JSON.stringify(data)
         }
     );
-
     return await response.json();
 }
