a67568f990d45251685ef7e0d3a15e6a4f0a6285
Sodaqw <art.sychev9@gmail.com>
2026-05-13 14:49:26 +0400

    Update reviewApi.js

--- app/static/js/api/reviewApi.js
+++ app/static/js/api/reviewApi.js
@@ -1,19 +1,15 @@
+//API для отправки отзыва
 const API_URL = 'https://okorochki-est.onrender.com/api/reviews';
-
 export async function createReview(data) {
-
     const response = await fetch(
         API_URL,
         {
             method: 'POST',
-
             headers: {
                 'Content-Type': 'application/json'
             },
-
             body: JSON.stringify(data)
         }
     );
-
     return await response.json();
 }
