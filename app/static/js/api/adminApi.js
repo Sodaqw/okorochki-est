3c6e3b011ceb7439dab5df14e8b2359a38a47629
Sodaqw <art.sychev9@gmail.com>
2026-05-13 14:52:52 +0400

    Update adminApi.js

--- app/static/js/api/adminApi.js
+++ app/static/js/api/adminApi.js
@@ -1,33 +1,27 @@
 const API_URL = 'https://okorochki-est.onrender.com/api/admin';
-
+//Функция для получения заявок
 export async function loadAllApplications() {
-
     const response = await fetch(
         `${API_URL}/applications`
     );
-
     return await response.json();
 }
-
+//Функция обновления статуса заявки
 export async function updateStatus(
     applicationId,
     status
 ) {
-
     const response = await fetch(
         `${API_URL}/applications/${applicationId}`,
         {
             method: 'PUT',
-
             headers: {
                 'Content-Type': 'application/json'
             },
-
             body: JSON.stringify({
                 status
             })
         }
     );
-
     return await response.json();
 }
