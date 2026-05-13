523b25eea86b1d0c26cd116f82e088836b60dd95
Sodaqw <art.sychev9@gmail.com>
2026-05-13 14:51:55 +0400

    Update adminAuthApi.js

--- app/static/js/api/adminAuthApi.js
+++ app/static/js/api/adminAuthApi.js
@@ -1,19 +1,15 @@
 const API_URL = 'https://okorochki-est.onrender.com/api/admin';
-
+//Функция для входа администратора
 export async function adminLogin(data) {
-
     const response = await fetch(
         `${API_URL}/login`,
         {
             method: 'POST',
-
             headers: {
                 'Content-Type': 'application/json'
             },
-
             body: JSON.stringify(data)
         }
     );
-
     return await response.json();
 }
