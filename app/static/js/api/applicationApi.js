7af32893325d07d79b19178e0d170b829383418a
Sodaqw <art.sychev9@gmail.com>
2026-05-13 14:51:27 +0400

    Update applicationApi.js

--- app/static/js/api/applicationApi.js
+++ app/static/js/api/applicationApi.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://okorochki-est.onrender.com/api/applications';
-
+///Функция для создания заявки на курс
 export async function createApplication(data) {
     const response = await fetch(
         API_URL,
@@ -13,6 +13,7 @@ export async function createApplication(data) {
     );
     return await response.json();
 }
+//Функция загрузки заявок
 export async function loadApplications(userId) {
     const response = await fetch(
         `${API_URL}/${userId}`
